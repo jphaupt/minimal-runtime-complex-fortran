9896f9f970026305f0c3050ae8fedd00622b1b28
jphaupt <jphaupt@protonmail.com>
2022-09-30 14:32:49 +0200

    silly main function

--- main.f90
+++ main.f90
@@ -1,19 +1,22 @@
 program main
-    use iso_fortran_env, only: stdout => output_unit
-    use real_or_complex_mod, only: number_t, real_number_t
-    implicit none(type, external)
-    class(number_t), allocatable :: x
-    real :: a = 8.
+    !! NOTE: defunct code, as it's been modified since writing the speed test
+    !! benchmarks
+    print*, "do make speed_test; this script currently doesn't do anything"
+    ! use iso_fortran_env, only: stdout => output_unit
+    ! use real_or_complex_mod, only: number_t, real_number_t
+    ! implicit none(type, external)
+    ! class(number_t), allocatable :: x
+    ! real :: a = 8.
 
-    x = real_number_t(3.)
+    ! x = real_number_t(3.)
 
-    x = x + 1.
+    ! x = x + 1.
 
-    x = x + x
+    ! x = x + x
 
-    call x%print()
+    ! call x%print()
 
-    print*, a, 1, 2, 3
-    print*, x%string(), 1, 2, 3
+    ! print*, a, 1, 2, 3
+    ! print*, x%string(), 1, 2, 3
 
 end program
